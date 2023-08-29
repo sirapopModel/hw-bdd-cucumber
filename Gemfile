42de59879752b8f4b4baa97b7f8c649cf6c9737b
sirapopModel <model12500@gmail.com>
2023-08-29 14:45:35 +0700

    part1 completed

--- Gemfile
+++ Gemfile
@@ -28,6 +28,14 @@ group :development, :test do
   gem 'sqlite3', '~> 1.3.6'
 end
 
+group :test do
+  gem 'cucumber-rails', :require => false
+  gem 'cucumber-rails-training-wheels' # some pre-fabbed step definitions  
+  gem 'database_cleaner' # to clear Cucumber's test database between runs
+  gem 'capybara'         # lets Cucumber pretend to be a web browser
+  gem 'launchy'          # a useful debugging aid for user stories
+end
+
 group :production do
   gem 'pg', '~> 0.2'
   gem 'rails_12factor'
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,9 +13,14 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+    when /^the RottenPotatoes home \s?page$/
+       '/movies'
+    when /^the Create New Movie \s?page$/
+      '/movies/new'
+    # when "the RottenPotatoes home page"
+    #   '/movies'
 
+    
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
@@ -27,9 +32,11 @@ module NavigationHelpers
         page_name =~ /^the (.*) page$/
         path_components = $1.split(/\s+/)
         self.send(path_components.push('path').join('_').to_sym)
+
       rescue NoMethodError, ArgumentError
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
           "Now, go and add a mapping in #{__FILE__}"
+
       end
     end
   end
