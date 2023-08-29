2373ba58dc466244940838bbe766172acae68a2c
sirapopModel <model12500@gmail.com>
2023-08-29 18:23:31 +0700

    part3 completed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,13 +2,16 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
+
   expect(Movie.count).to eq n_seeds.to_i
 end
 
@@ -19,28 +22,44 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
   pending "Fill in this step in movie_steps.rb"
+  
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: "(.*)"/ do |uncheck, rating|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  pending "Fill in this step in movie_steps.rb"
+  #pending "Fill in this step in movie_steps.rb"
+  rating_list = rating.split(",")
+  if !uncheck
+    rating_list.each {|x| steps %Q{When I check "#{x}" } }
+  else
+    rating_list.each {|x| steps %Q{When I uncheck "#{x}" }}
+  end
+
 end
 
 # Part 2, Step 3
-Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
+Then /^I should (not )?see the following movies: (.*)$/ do |no, movie|
+  movie_list = movie.split(",")
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  pending "Fill in this step in movie_steps.rb"
+  if !no
+    movie_list.each {|x| %Q{I should see "#{x}"}}
+  else
+    movie_list.each {|x| %Q{I should not see "#{x}"}}
+  end
+  #pending "Fill in this step in movie_steps.rb"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  pending "Fill in this step in movie_steps.rb"
+  title_list = Movie.select(:title)
+  title_list.each {|x| %Q{I should see "#{x}"}}
+  #pending "Fill in this step in movie_steps.rb"
 end
 
 ### Utility Steps Just for this assignment.
@@ -62,5 +81,7 @@ Then /complete the rest of of this scenario/ do
   # This shows you what a basic cucumber scenario looks like.
   # You should leave this block inside movie_steps, but replace
   # the line in your scenarios with the appropriate steps.
+  
+
   fail "Remove this step from your .feature files"
 end
